0b4ec2ea2540814a08ac33ff3d3811ae722dc0eb
luthfianesa <lutvheanesa@gmail.com>
2022-12-04 08:35:54 +0700

    Add Our Services Section

--- src/components/OurServices.js
+++ src/components/OurServices.js
@@ -1,9 +1,24 @@
 import "./ourServices.css"
+import Woman from "../assets/img_service.png"
+import Eclipse from "../assets/ellipse22.png"
 
-const OurServices = () => {
+const OurServices = (props) => {
   return (
-    <div>
-
+    <div id="ourServices" className="ourServices-container">
+      <div className="img-ourServices-container">
+        <img src={Woman}></img>
+      </div>
+      <div className="txt-ourServices-container">
+      <h1>Best Car Rental for any kind of trip in (Lokasimu)!</h1>
+        <p className="p-1">Sewa mobil di (Lokasimu) bersama Binar Car Rental jaminan harga lebih murah dibandingkan yang lain, kondisi mobil baru, serta kualitas pelayanan terbaik untuk perjalanan wisata, bisnis, wedding, meeting, dll.</p>
+        {/* Nerima data dari homepage */}
+        {props.dataOurServices.map((items) => {
+          return (<div className="looped-txt">
+            <div><img src={Eclipse}alt="eclipse-checklist" className="eclipse"></img></div>
+            <p className="p-2">{items.text}</p>
+          </div>)
+        })}
+        </div>
     </div>
   );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,42 @@
 import "./pages.css"
 import NavigationBar from "../components/NavigationBar";
 import Header from "../components/Header";
+import OurServices from "../components/OurServices";
+
+import { useState } from "react";
 
 const HomePage = () => {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      text: "Sewa Mobil dengan Supir di Bali 12 Jam",
+    },
+    {
+      id: 2,
+      text: "Sewa Mobil Lepas Kunci di Bali 24 Jam",
+    },
+    {
+      id: 3,
+      text: "Sewa Mobil Jangka Panjang Bulanan",
+    },
+    {
+      id: 4,
+      text: "Gratis Antar - Jemput Mobil di Bandara",
+    },
+    {
+      id: 5,
+      text: "Layanan Airport Transfer / Drop In Out",
+    },
+  ]);
   
   return (
     <div>
       <div className="container-for-header-and-navbar">
         <NavigationBar />
         <Header />
+        {/* Lempar data ke OurServices */}
       </div>
+        <OurServices dataOurServices={data}/>
     </div>
   );
 };
