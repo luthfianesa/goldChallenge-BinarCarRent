e74c511768f75088a94c9921cc309ffb2a288595
luthfianesa <lutvheanesa@gmail.com>
2022-12-04 13:38:01 +0700

    Add Navigation Bar, Header and Footer to Search Car Page

--- src/pages/SearchCar.js
+++ src/pages/SearchCar.js
@@ -1,9 +1,14 @@
 import "./pages.css"
+import NavigationBar from "../components/NavigationBar";
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 const SearchCar = () => {
   return (
     <div>
-
+      <NavigationBar />
+      <Header />
+      <Footer />
     </div>
   );
 };
